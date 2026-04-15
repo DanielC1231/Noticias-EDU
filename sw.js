3ec2d441014793a8c6f9d03f1d1b72ea63541a9e
DanielC1231 <cuellar.daniel@juanpablo2.edu.mx>
2026-04-14 23:35:01 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'noticias-edu-v1';
+const REPO_NAME = '/Noticias-EDU';
+
+const urlsToCache = [
+    REPO_NAME + '/',
+    REPO_NAME + '/index.html',
+    REPO_NAME + '/style.css',
+    REPO_NAME + '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
