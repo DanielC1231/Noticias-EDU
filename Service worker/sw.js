b0ffd99a89b89f9b225eddc43462a37e17387143
DanielC1231 <cuellar.daniel@juanpablo2.edu.mx>
2026-04-14 23:23:26 -0700

    Add files via upload

---
+++ Service worker/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'noticias-app-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/manifest.json'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activar
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Responder con caché o red
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
